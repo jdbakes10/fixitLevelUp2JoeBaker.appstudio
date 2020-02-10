a171f212311a9f436d268cb84ed5f012702aed5e
jdbakes10 <jdb41193@creighton.edu>
2020-02-09 23:53:38 -0600

    final changes
    final changes

--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -1,3 +1,4 @@
+
 var firstName = prompt(`What is your first name?`)
 var userState = prompt(`What is your state abbreviation?`)
 var userTempature = Number(prompt(`What is the tempature in degrees Farenheit?`))
@@ -9,6 +10,7 @@ var nameCapital = firstName.charAt(0).toUpperCase() +firstName.slice(1)
 //creates an array holding messages
 var userInfo = [`${nameCapital}`,`${stateCapital}`,`${userTempature}`]
 console.log(`${userInfo}`)
+
 
 //outputs a specific message based on state and temp
 switch(true) {
@@ -25,3 +27,5 @@ case( stateCapital == 'FL' && userTempature < 70 && userTempature >50):
   NSB.MsgBox(`${nameCapital}, wear a warm coat, hat and gloves, Maybe a scarf.`)
   break
 }
+
+//done
